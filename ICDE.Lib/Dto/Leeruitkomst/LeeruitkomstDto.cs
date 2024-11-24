0a1ff7f1a3bedde595af82246314a1da89826530
CRNHV <44652665+CRNHV@users.noreply.github.com>
2024-11-24 21:35:18 +0100

    Push ignored objects

---
+++ ICDE.Lib/Dto/Leeruitkomst/LeeruitkomstDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ICDE.Lib.Dto.Leeruitkomst;
+public sealed class LeeruitkomstDto
+{
+    public int Id { get; set; }
+    public string Naam { get; set; }
+    public string Beschrijving { get; set; }
+    public int VersieNummer { get; set; }
+}
