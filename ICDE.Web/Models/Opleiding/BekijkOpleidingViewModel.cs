b8e9b20c14de3fccafa6046e65e3661992dc26b0
CRNHV <44652665+CRNHV@users.noreply.github.com>
2024-11-30 17:13:20 +0100

    Add UC10 and partially UC11

---
+++ ICDE.Web/Models/Opleiding/BekijkOpleidingViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using ICDE.Lib.Dto.Opleidingen;
+using ICDE.Lib.Dto.Vak;
+
+namespace ICDE.Web.Models.Opleiding;
+
+public class BekijkOpleidingViewModel
+{
+    public OpleidingDto Opleiding { get; set; }
+    public List<VakDto> BeschikbareVakken { get; set; } = new();
+    public List<OpleidingDto> EerdereVersies { get; set; }
+}
