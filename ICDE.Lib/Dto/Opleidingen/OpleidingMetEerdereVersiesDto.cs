aae1c432be757b0e42e0f1ce8d9d95ac80e9b719
CRNHV <44652665+CRNHV@users.noreply.github.com>
2024-11-30 19:31:12 +0100

    Add ignored files

---
+++ ICDE.Lib/Dto/Opleidingen/OpleidingMetEerdereVersiesDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICDE.Lib.Dto.Opleidingen;
+public class OpleidingMetEerdereVersiesDto
+{
+    public OpleidingDto OpleidingDto { get; set; }
+    public List<OpleidingDto> EerdereVersies { get; set; }
+
+}
