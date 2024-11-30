59b3631542b5304726f55217aba7ad8f50207b56
CRNHV <44652665+CRNHV@users.noreply.github.com>
2024-11-30 19:25:49 +0100

    Add cursus view

--- ICDE.Web/Controllers/Auteur/CursusController.cs
+++ ICDE.Web/Controllers/Auteur/CursusController.cs
@@ -1,11 +1,43 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ICDE.Lib.Dto.Cursus;
+using ICDE.Lib.Services.Interfaces;
+using ICDE.Web.Models.Cursus;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ICDE.Web.Controllers.Auteur;
 
-[Route("auteur/cursussen")]
+[Route("auteur/cursus")]
 public class CursusController : ControllerBase
 {
+    private readonly ICursusService _cursusService;
+
+    public CursusController(ICursusService cursusService)
+    {
+        _cursusService = cursusService;
+    }
+
+    [HttpGet("Index")]
+    public async Task<IActionResult> Index()
+    {
+        List<CursusDto> cursussen = await _cursusService.GetAll();
+
+        return View("/Views/Auteur/Cursus/Index.cshtml", cursussen);
+    }
+
+    [HttpGet("get/{cursusGroupId}")]
+    public async Task<IActionResult> BekijkCursus([FromRoute] Guid cursusGroupId)
+    {
+        CursusMetPlanningDto cursus = await _cursusService.GetFullCursusByGroupId(cursusGroupId);
+        List<CursusDto> eerdereVersies = await _cursusService.GetEarlierVersionsByGroupId(cursusGroupId, cursus.Id);
+        return View("/Views/Auteur/Cursus/BekijkCursus.cshtml", new BekijkCursusViewModel()
+        {
+            Cursus = cursus,
+            EerderVersies = eerdereVersies
+        });
+    }
+
     /// <summary>
     /// UC17
     /// </summary>
