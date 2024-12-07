fb54a33560a8f9fafce132e86d2acf475d6fe91b
CRNHV <44652665+CRNHV@users.noreply.github.com>
2024-12-07 14:34:22 +0100

    Add ignored DTO

---
+++ ICDE.Lib/Dto/Leeruitkomst/LeeruitkomstMetEerdereVersiesDto.cs
@@ -0,0 +1,6 @@
+﻿namespace ICDE.Lib.Dto.Leeruitkomst;
+public class LeeruitkomstMetEerdereVersiesDto
+{
+    public LeeruitkomstDto Leeruitkomst { get; set; }
+    public List<LeeruitkomstDto> EerdereVersies { get; set; } = new List<LeeruitkomstDto>();
+}
