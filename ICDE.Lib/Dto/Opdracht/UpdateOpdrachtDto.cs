5cf2fba700ff18d736148cbf7e525ccbddc677cb
CRNHV <44652665+CRNHV@users.noreply.github.com>
2024-12-14 15:36:28 +0100

    Update dto's

---
+++ ICDE.Lib/Dto/Opdracht/UpdateOpdrachtDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ICDE.Lib.Dto.Opdracht;
+public class UpdateOpdrachtDto
+{
+    public string Naam { get; set; }
+    public string Beschrijving { get; set; }
+    public bool IsToets { get; set; }
+    public Guid GroupId { get; set; }
+}
---
+++ ICDE.Lib/Dto/Vak/MaakVakDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICDE.Lib.Dto.Vak;
+public class MaakVakDto
+{
+}
