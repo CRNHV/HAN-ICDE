89f1f542fd242eed8f1c7ea01319a981d6aaf635
jordysmeenge <167679808+jordysmeenge@users.noreply.github.com>
2024-12-15 19:51:34 +0100

    uc20

--- ICDE.Web/Controllers/Student/CursusController.cs
+++ ICDE.Web/Controllers/Student/CursusController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using ICDE.Lib.Domain.User;
+using ICDE.Lib.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,15 +11,33 @@ namespace ICDE.Web.Controllers.Student;
 [Authorize(Roles = UserRole.Student)]
 public class CursusController : ControllerBase
 {
+    private readonly ICursusService _cursusService;
+
+    public CursusController(ICursusService cursusService)
+    {
+        _cursusService = cursusService;
+    }
+
+    [HttpGet("Index")]
+    public async Task<IActionResult> Index()
+    {
+        var cursussen = await _cursusService.AlleUnieke();
+        return View("/Views/Student/Cursus/Index.cshtml", cursussen);
+    }
+
     /// <summary>
     /// UC20
     /// </summary>
     /// <param name="cursusGroupId"></param>
     /// <returns></returns>
-    [HttpGet("{cursusGroupId}")]
+    [HttpGet("get/{cursusGroupId}")]
     public async Task<IActionResult> BekijkCursus([FromRoute] Guid cursusGroupId)
     {
-        // Haal cursus incl planning op
-        return View();
+        var cursus = await _cursusService.HaalAlleDataOp(cursusGroupId);
+        if (cursus == null)
+        {
+            return NotFound();
+        }
+        return View("/Views/Student/Cursus/BekijkCursus.cshtml", cursus);
     }
 }
